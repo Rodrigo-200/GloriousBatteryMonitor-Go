2836348300b37b34e9a2e5100b34e963e6156d4d
Rodrigo Martins <84940356+Rodrigo-200@users.noreply.github.com>
2025-10-16 00:01:01 +0100

    Bump version from 2.2.4 to 2.2.5 (#22)

--- main.go
+++ main.go
@@ -81,7 +81,7 @@ type Settings struct {
 	CriticalBatteryThreshold int  `json:"criticalBatteryThreshold"` // percentage
 }
 
-const currentVersion = "2.2.4"
+const currentVersion = "2.2.5"
 
 var (
 	device            *hid.Device
